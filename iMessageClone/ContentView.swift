db5db1b034c16ace19dfed50024254cec9607ff7
Amos Gyamfi <amosgyamfi.fi@gmail.com>
2021-12-13 15:54:20 +0200

    Initial Commit

---
+++ iMessageClone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iMessageClone
+//
+//  Created by amos.gyamfi@getstream.io on 13.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iMessageClone/iMessageCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMessageCloneApp.swift
+//  iMessageClone
+//
+//  Created by amos.gyamfi@getstream.io on 13.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct iMessageCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
